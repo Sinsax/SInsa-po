021061aae5282c733e38c10d46f70345a339df50
sinsax <2498349501@qq.com>
2025-08-17 11:34:38 +0800

    clear default page

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "SInsa-po",
-	subtitle: "Demo Site",
+	subtitle: "记录/分享",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -70,14 +70,14 @@ export const profileConfig: ProfileConfig = {
 		// 	url: "https://store.steampowered.com",
 		// },
 		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
-		},
-		{
 			name: "Bilibili",
 			icon: "fa6-brands:bilibili",
 			url: "https://space.bilibili.com/8567742",
+		},
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/Sinsax",
 		},
 	],
 };
--- src/content/posts/first/index.md
+++ src/content/posts/first/index.md
@@ -13,3 +13,6 @@ lang: ''
 
 比如rust学习的记录，blender的一些成果/脚本，一些事情
 
+## Bilibili
+
+<iframe width="100%" height="468" src="//player.bilibili.com/player.html?bvid=BV1xJJLzWE9i&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
