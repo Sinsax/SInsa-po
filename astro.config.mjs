523d86a2e37c1297e92d498a16cf2452b4880bea
Sinsax <34959437+Sinsax@users.noreply.github.com>
2025-08-17 17:08:17 +0800

    SInsa-po

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sinsax.github.io",
-	base: "/sinsax.github.io",
+	base: "/SInsa-po",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
