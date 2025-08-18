7eb60c01ba5119cef71b2b7ed16890addad0d623
sinsa <2498349501@qq.com>
2025-08-18 08:50:48 +0800

    /sinsa-po

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sinsax.github.io",
-	base: "/",
+	base: "/SInsa-po",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
